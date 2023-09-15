9b446907e3dee8e6d4dce64a450fa25a6f204a89
Ro-Nel <69656274+Ro-Nel@users.noreply.github.com>
2023-09-14 20:11:06 -0400

    Cargado de Archivos
    Creacion del proyeccto u sibido al guit

---
+++ PracCont/src/main/java/pracconte/PracCont/PracContApplication.java
@@ -0,0 +1,13 @@
+package pracconte.PracCont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracContApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracContApplication.class, args);
+	}
+
+}
---
+++ PracCont/src/test/java/pracconte/PracCont/PracContApplicationTests.java
@@ -0,0 +1,13 @@
+package pracconte.PracCont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracContApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
